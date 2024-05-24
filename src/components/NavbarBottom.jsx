873acc7ba8b7655d5802d7fccfad920ec9bad2c4
tektoss <140408137+tektoss@users.noreply.github.com>
2024-05-23 23:28:36 -0500

    Update NavbarBottom.jsx

--- src/components/NavbarBottom.jsx
+++ src/components/NavbarBottom.jsx
@@ -43,7 +43,7 @@ export default function NavbarBottom() {
               { appName }
             </span> */}
           </Link>
-          <p style={{ fontSize: '11px' }}>Your Marketplace for Vehicles and Electronic Gadgets</p>
+          <p style={{ fontSize: '11px' }}>A Multi-purpose Place</p>
         </div>
         <div className="bottom-nav__content-search-div">
           <input
