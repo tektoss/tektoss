d65a9cb58433c5c5635a798ba2e16d9aa14c41bb
tektoss <140408137+tektoss@users.noreply.github.com>
2025-02-28 14:21:21 -0600

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import Navbar from '../../components/Navbar';
-// import Footer from '../../sections/Footer';
 import Main from '../../sections/HomeMain';
 import appName from '../../Constants/constantVariables';
 import AuthFooter from '../../auth/components/AuthFooter';
@@ -17,11 +16,22 @@ export default function Home() {
           Discover the Best Deals, Connect with Sellers, and Trade Gadgets with Ease right here on ${appName}.`}
         />
         <link rel="canonical" href="/" />
+        {/* Google Analytics Tag */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-7DH8N2G5K2"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-7DH8N2G5K2');
+          `}
+        </script>
       </Helmet>
+      
       <Navbar />
       <Main />
-      {/* <Footer /> */}
       <AuthFooter />
     </>
   );
 }
+
